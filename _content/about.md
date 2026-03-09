8e75012a2b73eaea6b364e074eee8c36b10c612f
anjs21 <anooj.sathyan11@gmail.com>
2026-03-09 16:10:04 +0100

    UI overhaul

--- _content/about.md
+++ _content/about.md
@@ -1,12 +1,12 @@
 <h2 style="font-weight: 700;">Welcome to AltraVia</h2>
 <!--<h2>Welcome to AltraVia</h2>
 <p class="lead">I'm Neema, and I help students like you navigate the path to a successful master's degree and career in Europe.</p>-->
-<div class="row align-items-center">
+<div class="row align-items-center justify-content-center bg-light">
   <div class="col-lg-3 text-center">
     <img class="img-fluid py-3" src="/img/Neema.jpg" alt="Profile Picture">
   </div>
-  <div class="col-lg-9 text-start">
-    <h2 style="font-weight: 700;">Welcome to AltraVia</h2>
+  <div class="col-lg-6 text-start">
+    <!-- <h2 style="font-weight: 700;">Welcome to AltraVia</h2> -->
     <p>
       I am Neema Tharayil Salim, founder of AltraVia. My mission is to empower biomedical aspirants to explore opportunities in higher education and professional careers. I hold a bachelor’s degree in Biomedical Engineering and earned a fully funded merit‑based scholarship to pursue my master’s in Bionics Engineering, specializing in Medical Robotics, at the University of Pisa and Scuola Superiore Sant’Anna in Italy. Currently, I work as a System Test Engineer in a German medical robotics company. My objective is to share my experience to guide, inspire, and support the next generation of biomedical professionals - helping them build confident careers and seize opportunities worldwide.
     </p>
--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 // Scrolls to the selected menu item on the page
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+$(function () {
+  $('a[href*=#]:not([href=#])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
       var target = $(this.hash);
@@ -15,29 +15,29 @@ $(function() {
   });
 });
 
-// Change navbar style on scroll
-$(document).ready(function() {
-  var mainNav = $("#mainNav");
-  var collapseElement = document.getElementById('bs-example-navbar-collapse-1');
+// Change navbar style on scroll (not used in new theme)
+// $(document).ready(function() {
+//   var mainNav = $("#mainNav");
+//   var collapseElement = document.getElementById('bs-example-navbar-collapse-1');
 
-  // Function to add/remove class
-  var navbarCollapse = function() {
-    if (mainNav.offset().top > 250) {
-      mainNav.addClass("bg-dark");
-    } else {
-      // Only remove bg-dark if the navbar is not collapsed
-      if (!$(collapseElement).hasClass('show')) {
-        mainNav.removeClass("bg-dark");
-      }
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+//   // Function to add/remove class
+//   var navbarCollapse = function() {
+//     if (mainNav.offset().top > 250) {
+//       mainNav.addClass("bg-dark");
+//     } else {
+//       // Only remove bg-dark if the navbar is not collapsed
+//       if (!$(collapseElement).hasClass('show')) {
+//         mainNav.removeClass("bg-dark");
+//       }
+//     }
+//   };
+//   // Collapse now if page is not at top
+//   navbarCollapse();
+//   // Collapse the navbar when page is scrolled
+//   $(window).scroll(navbarCollapse);
 
-  // Add dark background when navbar is expanded on mobile
-  $(collapseElement).on('show.bs.collapse', function () { mainNav.addClass('bg-dark'); });
-  // Remove dark background when navbar is collapsed (if not scrolled)
-  $(collapseElement).on('hide.bs.collapse', function () { if (mainNav.offset().top <= 250) { mainNav.removeClass('bg-dark'); } });
-});
+//   // Add dark background when navbar is expanded on mobile
+//   $(collapseElement).on('show.bs.collapse', function () { mainNav.addClass('bg-dark'); });
+//   // Remove dark background when navbar is collapsed (if not scrolled)
+//   $(collapseElement).on('hide.bs.collapse', function () { if (mainNav.offset().top <= 250) { mainNav.removeClass('bg-dark'); } });
+// });
